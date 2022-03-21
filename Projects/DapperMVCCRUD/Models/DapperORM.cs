8dd5a55d2937872b04a184175b36bb7244dda1b2
Niruxaben Padhiyar <nilipadhiyar293@outlook.com>
2022-03-21 12:30:44 -0600

    person details added

---
+++ Projects/DapperMVCCRUD/Models/DapperORM.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DapperMVCCRUD.Models
+{
+    public static class DapperORM
+    {
+        private static string connectionString = @"Data Source = DESKTOP-BLE4GH5\SQL2019TRAINING;Initial Catalog=MVCDatabase;Integrated Security=True;";
+
+        public static void ExecuteWithoutReturn(string procedureName, DynamicParameters param)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                con.Execute(procedureName, param, commandType : CommandType.StoredProcedure);
+            }
+        }
+
+        //DapperORM.ExecuteReturnScalar<int>(_,_);
+        public static T ExecuteReturnScalar<T>(string procedureName, DynamicParameters param)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+               return (T) Convert.ChangeType(con.Execute(procedureName, param, commandType: CommandType.StoredProcedure),typeof(T));
+            }
+        }
+        //DapperORM.returnList<PersonModel> <= IEnumerable<Person>
+        public static IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                return con.Query<T>(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
---
+++ Projects/DapperMVCCRUD/Models/PersonModel.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DapperMVCCRUD.Models
+{
+    public class PersonModel
+    {
+        public int PersonID { get; set; }
+
+        [Required(ErrorMessage =" First Name required !!")]
+        [Display(Name = "First Name")]
+
+        public string FirstName { get; set; }
+
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = " Email Address required !!")]
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage ="Enter a valid email address!")]
+        public string EmailID { get; set; }
+        public string Gender { get; set; }
+
+        public int AddressID { get; set; }
+
+
+
+
+
+    }
+}
